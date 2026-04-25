d34c387966eb23810c7d0783b5996c64e7820e9b
jo11y-rog3r <armedgandalf@gmail.com>
2026-04-25 13:36:12 +0100

    first commit

---
+++ colour-v1.4/capture.py
@@ -0,0 +1,4 @@
+from PIL import ImageGrab
+
+def grab_screen_data():
+    return ImageGrab.grab()
---
+++ colour-v1.4/config.py
@@ -0,0 +1,15 @@
+TICK_RATE = 0.20 # every 200 milliseconds (ms)
+
+GRID_COLS = 8
+GRID_ROWS = 6
+
+"""
+RANDOM_NUM_SAMPLES
+Dictates how many samples will be collected when using the 
+"Random" colour detection mode.
+"""
+RANDOM_NUM_SAMPLES = 50
+
+# Do not edit the below values.
+SIGNAL_ON = str("ON\n")
+SIGNAL_OFF = str("OFF\n")
